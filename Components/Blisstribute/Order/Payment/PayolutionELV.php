cf7f4a2a69ea945986d81b9133925a831e41e690
roman <roman.robel@exitb.de>
2017-03-30 14:10:39 +0200

    * version update

--- Components/Blisstribute/Order/Payment/PayolutionELV.php
+++ Components/Blisstribute/Order/Payment/PayolutionELV.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/AbstractExternalPayment.php';
  * @copyright Copyright (c) 2016
  * @since     1.0.0
  */
-class Shopware_Components_Blisstribute_Order_Payment_Payolution
+class Shopware_Components_Blisstribute_Order_Payment_PayolutionELV
     extends Shopware_Components_Blisstribute_Order_Payment_AbstractExternalPayment
 {
     /**
