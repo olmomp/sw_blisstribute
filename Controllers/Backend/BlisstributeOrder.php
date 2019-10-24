c50d849c1e2a8c20f7880ae3fce4392eefca5d3e
Cihan Tas <cihan.tas@exitb.de>
2019-10-24 15:51:17 +0200

    Fix search.

--- Controllers/Backend/BlisstributeOrder.php
+++ Controllers/Backend/BlisstributeOrder.php
@@ -35,6 +35,8 @@ class Shopware_Controllers_Backend_BlisstributeOrder extends Shopware_Controller
      */
     private $plugin;
 
+    protected $filterFields = ['qqqqqqqq'];
+
     /**
      * {@inheritdoc}
      */
